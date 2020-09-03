8161136534e9c4e7f62f647512853e9b8f6553e4
laurenarthur <arthur_lauren@columbusstate.edu>
2020-09-03 17:32:59 +0000

    Adding to the list  not working

--- javascript.js
+++ javascript.js
@@ -3,10 +3,20 @@ $(document).ready(function() {
 
   var UserInput = document.getElementById("userinput").value;
 
-  function addFunction(){
-      var li = <li> + UserInput + </li>;
+  var array = [UserInput];
 
-      document.getElementById("mylist").appendChild(li);
+  function addFunction(){
+    var list = document.createElement('ul');
+    for(i=0;array.length;i++){
+        var item = document.createElement('li');
+        item.appendChild(document.createTextNode(array[i]));
+
+        list.appendChild(item);
+
+    }
+    return mylist;
+      //var li = <li> + Array + </li>;
+      //document.getElementById("mylist").appendChild(li);
   }
   
 });
