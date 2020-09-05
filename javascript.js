952c18d6a0b3dabfeaedd3425ba1e5035e2f8baa
laurenarthur <69876588+laurenarthur@users.noreply.github.com>
2020-09-04 20:59:00 -0400

    Update javascript.js
    Added the remove function, not working but maybe close to if I can figure out how to read the list

--- javascript.js
+++ javascript.js
@@ -1,7 +1,28 @@
 document.getElementById("addButton").onclick = function(){
 	var UserInput = document.getElementById("userInput").value;
-	var li = "<li>" + UserInput + "</li>";
+	var li = document.createElement("li");
+	li.className = "list-group-item";
+	li.innerText = UserInput;
 
-	document.getElementById("mylist").innerHTML +=li;
+	document.getElementById("mylist").appendChild(li);
 	document.getElementById("userInput").value = "";
+}
+
+document.getElementById("removeButton").onclick = function(){
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('userInputtoDelete');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("mylist");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      ul.removeChild(li.childNodes[i]);
+    } else {
+      li[i].style.display = "none";
+    }
+  }
 }
