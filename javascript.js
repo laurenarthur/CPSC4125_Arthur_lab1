e7ad1101a8c8e41bedb367234ce375557e4a5d28
laurenarthur <arthur_lauren@columbusstate.edu>
2020-09-05 19:37:49 +0000

    Remove working

--- javascript.js
+++ javascript.js
@@ -9,20 +9,16 @@ document.getElementById("addButton").onclick = function(){
 }
 
 document.getElementById("removeButton").onclick = function(){
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('userInputtoDelete');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("mylist");
-  li = ul.getElementsByTagName('li');
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      ul.removeChild(li.childNodes[i]);
-    } else {
-      li[i].style.display = "none";
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('userInputtoDelete');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("mylist");
+    li = ul.getElementsByTagName('li');
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i];
+        if (a.textContent.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display="none"
+        } 
     }
-  }
 }
